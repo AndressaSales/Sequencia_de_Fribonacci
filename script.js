a04906dff0a71c497dad688faf706ce454f26a01
AndressaSales <andressaccs2001@gmail.com>
2023-09-11 14:35:28 -0300

    fribonacci

---
+++ script.js
@@ -0,0 +1,14 @@
+let anterior= 0;
+let atual = 1;
+let = proximo = 1;
+
+const numero = prompt('Digite um número:');
+document.write('Sequencia de Fibonacci');
+
+for (let i = 0; i < numero; i++){
+    document.write("<br>" +proximo);
+    anterior = atual + proximo;
+    atual = proximo
+    proximo = anterior
+
+}
